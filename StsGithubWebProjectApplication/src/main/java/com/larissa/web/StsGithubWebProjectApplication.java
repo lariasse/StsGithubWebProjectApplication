992b899ad09423080dc1ac4b7c493fdbb25b1c42
PC <PC@192.168.86.245>
2022-11-02 13:44:55 -0400

    My first commit inside

---
+++ StsGithubWebProjectApplication/src/main/java/com/larissa/web/StsGithubWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.larissa.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGithubWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGithubWebProjectApplication.class, args);
+	}
+
+}
---
+++ StsGithubWebProjectApplication/src/test/java/com/larissa/web/StsGithubWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissa.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsGithubWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
